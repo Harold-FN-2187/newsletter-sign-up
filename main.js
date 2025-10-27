b82a2295e65444dbca8f58d50ba3601f59ef6bf3
Harold_FN_2187 <laryeaharold319@gmail.com>
2025-10-27 22:11:15 +0000

    minor additions

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const subscriber = document.getElementById("submit");
+
+// Perform usual validation
+// Trigger and Open modal when validation is successful
+//Dismiss modal when button is clicked </mobile>
