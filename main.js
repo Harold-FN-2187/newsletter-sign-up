d0365835b4e563d560e6611aafcbe2b083d50be5
Harold_FN_2187 <laryeaharold319@gmail.com>
2025-10-28 22:38:43 +0000

    Opening foray into modal

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// const { StrictMode } = require("react");
+
 const form = document.querySelector("form");
 const email = document.getElementById("mail");
-const subscriber = document.getElementById("submit");
+const subscribe = document.getElementById("submit");
 
 // Perform usual validation
 // Trigger and Open modal when validation is successful
